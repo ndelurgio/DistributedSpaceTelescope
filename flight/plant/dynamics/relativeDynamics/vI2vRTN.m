37f4f16cbf7f91a4b3f3be6e3b8f4371f8866132
ndelurgio <delurgion@gmail.com>
2023-04-16 20:04:53 -0700

    add resources

--- flight/plant/dynamics/relativeDynamics/vI2vRTN.m
+++ flight/plant/dynamics/relativeDynamics/vI2vRTN.m
@@ -7,7 +7,7 @@ relativeState_rtn = zeros(6,1);
 relativeState_rtn(1:3,1) = R_eci2rtn * relativeState_I(1:3, 1);
 relativeState_rtn(4:6,1) = R_eci2rtn * relativeState_I(4:6, 1);
 % Transform velocity
-w_I2rtn = [0; 0; theta0_dot];
-relativeState_rtn(4:6,1) = relativeState_rtn(4:6,1) - cross(w_I2rtn, relativeState_rtn(1:3,1));
+w_I2rtn = [0; 0; -theta0_dot];
+relativeState_rtn(4:6,1) = relativeState_rtn(4:6,1) + cross(w_I2rtn, relativeState_rtn(1:3,1));
 
 end

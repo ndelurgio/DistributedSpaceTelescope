28ea7e131f199933a7a373d84f7bc97de92ce612
Hugo Buurmeijer <hugo.buurmeijer@gmail.com>
2023-04-21 11:43:28 -0700

    Compute integration constants HCW from initial conditions

---
+++ utilities/functions/computeHCWIntConstants.m
@@ -0,0 +1,17 @@
+function IC_hcw = computeHCWIntConstants(relativeCartesianState0, a, mu)
+% Compute the integration constants for the HCW equations using the initial
+% conditions
+
+n = meanMotion(mu, a);
+
+I_matrix = [a*ones(3), zeros(3); zeros(3), a*n*ones(3)];
+t_matrix = [1, 0, 1, 0, 0, 0;
+            0, 2, 0, 1, 0, 0;
+            0, 0, 0, 0, 0, 1;
+            0, 1, 0, 0, 0, 0;
+            -3/2, 0, -2, 0, 0, 0;
+            0, 0, 0, 0, 1, 0];
+
+IC_hcw = (I_matrix * t_matrix) \ relativeCartesianState0;
+
+end

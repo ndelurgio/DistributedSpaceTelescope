799bd548135d2da977b6451ebbcef3f8ac06efff
ndelurgio <delurgion@gmail.com>
2023-04-20 23:22:01 -0700

    merge

--- flight/plant/dynamics/absoluteDynamics/cartesianPropagatorPerturbed.m
+++ flight/plant/dynamics/absoluteDynamics/cartesianPropagatorPerturbed.m
@@ -1,6 +1,9 @@
 function cartesianStateDerivative = cartesianPropagatorPerturbed(cartesianState_J2000,accelPerturbed_J2000_mps2,gravitationParameter_m3_s2)
 
 cartesianStateDerivative = zeros(6,1);
+if norm(cartesianState_J2000(1:3,1)) == 0
+    return
+end
 cartesianStateDerivative(1:3,1) = cartesianState_J2000(4:6,1);
 
 r = norm(cartesianState_J2000(1:3));

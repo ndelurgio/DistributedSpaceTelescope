ceac3151cd3ecc4b833eb89562135b8a5aff7433
ndelurgio <delurgion@gmail.com>
2023-04-21 12:43:11 -0700

    fixed bug in integration constant calculation

--- utilities/config/ps3.m
+++ utilities/config/ps3.m
@@ -67,8 +67,7 @@ plant.deputy.initialConditions.cartesianState.velocityY_J2000_m = v_ijk(2);
 plant.deputy.initialConditions.cartesianState.velocityZ_J2000_m = v_ijk(3);
 clear r_ijk v_ijk
 
-%% Relative States
-% Relative elements of deputy with respect to chief
+%% Relative RTN States
 relativeState(1, 1) = plant.deputy.initialConditions.cartesianState.positionX_J2000_m - plant.chief.initialConditions.cartesianState.positionX_J2000_m;
 relativeState(2, 1) = plant.deputy.initialConditions.cartesianState.positionY_J2000_m - plant.chief.initialConditions.cartesianState.positionY_J2000_m;
 relativeState(3, 1) = plant.deputy.initialConditions.cartesianState.positionZ_J2000_m - plant.chief.initialConditions.cartesianState.positionZ_J2000_m;
@@ -76,20 +75,12 @@ relativeState(4, 1) = plant.deputy.initialConditions.cartesianState.velocityX_J2
 relativeState(5, 1) = plant.deputy.initialConditions.cartesianState.velocityY_J2000_m - plant.chief.initialConditions.cartesianState.velocityY_J2000_m;
 relativeState(6, 1) = plant.deputy.initialConditions.cartesianState.velocityZ_J2000_m - plant.chief.initialConditions.cartesianState.velocityZ_J2000_m;
 relativeState = vI2vRTN(relativeState, theta0_dot, R_eci2rtn);
-
 plant.deputy.initialConditions.relativeCartesianState.positionX_RTN_m = relativeState(1);
 plant.deputy.initialConditions.relativeCartesianState.positionY_RTN_m = relativeState(2);
 plant.deputy.initialConditions.relativeCartesianState.positionZ_RTN_m = relativeState(3);
 plant.deputy.initialConditions.relativeCartesianState.velocityX_RTN_m = relativeState(4);
 plant.deputy.initialConditions.relativeCartesianState.velocityY_RTN_m = relativeState(5);
 plant.deputy.initialConditions.relativeCartesianState.velocityZ_RTN_m = relativeState(6);
-
-plant.deputy.initialConditions.orbitElementDifferences.deltaSemiMajorAxis_m             = plant.deputy.initialConditions.orbitElements.semiMajorAxis_m - plant.chief.initialConditions.orbitElements.semiMajorAxis_m;
-plant.deputy.initialConditions.orbitElementDifferences.deltaEccentricity                = plant.deputy.initialConditions.orbitElements.eccentricity - plant.chief.initialConditions.orbitElements.eccentricity;
-plant.deputy.initialConditions.orbitElementDifferences.deltaInclination_rad             = plant.deputy.initialConditions.orbitElements.inclination_rad - plant.chief.initialConditions.orbitElements.inclination_rad;
-plant.deputy.initialConditions.orbitElementDifferences.deltaLongitudeAscendingNode_rad  = plant.deputy.initialConditions.orbitElements.longitudeAscendingNode_rad - plant.chief.initialConditions.orbitElements.longitudeAscendingNode_rad;
-plant.deputy.initialConditions.orbitElementDifferences.deltaArgumentPerigee_rad         = plant.deputy.initialConditions.orbitElements.argumentPerigee_rad - plant.chief.initialConditions.orbitElements.argumentPerigee_rad;
-plant.deputy.initialConditions.orbitElementDifferences.deltaMeanAnomaly_rad             = plant.deputy.initialConditions.orbitElements.MeanAnomaly_rad - plant.chief.initialConditions.orbitElements.MeanAnomaly_rad;
 
 plant.chief.initialConditions.relativeCartesianState.positionX_RTN_m = -relativeState(1);
 plant.chief.initialConditions.relativeCartesianState.positionY_RTN_m = -relativeState(2);
@@ -98,6 +89,17 @@ plant.chief.initialConditions.relativeCartesianState.velocityX_RTN_m = -relative
 plant.chief.initialConditions.relativeCartesianState.velocityY_RTN_m = -relativeState(5);
 plant.chief.initialConditions.relativeCartesianState.velocityZ_RTN_m = -relativeState(6);
 
+clear relativeState
+
+%% Orbit Element Differences
+
+plant.deputy.initialConditions.orbitElementDifferences.deltaSemiMajorAxis_m             = plant.deputy.initialConditions.orbitElements.semiMajorAxis_m - plant.chief.initialConditions.orbitElements.semiMajorAxis_m;
+plant.deputy.initialConditions.orbitElementDifferences.deltaEccentricity                = plant.deputy.initialConditions.orbitElements.eccentricity - plant.chief.initialConditions.orbitElements.eccentricity;
+plant.deputy.initialConditions.orbitElementDifferences.deltaInclination_rad             = plant.deputy.initialConditions.orbitElements.inclination_rad - plant.chief.initialConditions.orbitElements.inclination_rad;
+plant.deputy.initialConditions.orbitElementDifferences.deltaLongitudeAscendingNode_rad  = plant.deputy.initialConditions.orbitElements.longitudeAscendingNode_rad - plant.chief.initialConditions.orbitElements.longitudeAscendingNode_rad;
+plant.deputy.initialConditions.orbitElementDifferences.deltaArgumentPerigee_rad         = plant.deputy.initialConditions.orbitElements.argumentPerigee_rad - plant.chief.initialConditions.orbitElements.argumentPerigee_rad;
+plant.deputy.initialConditions.orbitElementDifferences.deltaMeanAnomaly_rad             = plant.deputy.initialConditions.orbitElements.MeanAnomaly_rad - plant.chief.initialConditions.orbitElements.MeanAnomaly_rad;
+
 plant.chief.initialConditions.orbitElementDifferences.deltaSemiMajorAxis_m             = -plant.deputy.initialConditions.orbitElementDifferences.deltaSemiMajorAxis_m;
 plant.chief.initialConditions.orbitElementDifferences.deltaEccentricity                = -plant.deputy.initialConditions.orbitElementDifferences.deltaEccentricity;
 plant.chief.initialConditions.orbitElementDifferences.deltaInclination_rad             = -plant.deputy.initialConditions.orbitElementDifferences.deltaInclination_rad;
@@ -105,6 +107,27 @@ plant.chief.initialConditions.orbitElementDifferences.deltaLongitudeAscendingNod
 plant.chief.initialConditions.orbitElementDifferences.deltaArgumentPerigee_rad         = -plant.deputy.initialConditions.orbitElementDifferences.deltaArgumentPerigee_rad;
 plant.chief.initialConditions.orbitElementDifferences.deltaMeanAnomaly_rad             = -plant.deputy.initialConditions.orbitElementDifferences.deltaMeanAnomaly_rad;
 clear relativeState
+
+%% Integration Constants
+HCWIntConstantsDeputy = computeHCWIntConstants([plant.deputy.initialConditions.relativeCartesianState.positionX_RTN_m,plant.deputy.initialConditions.relativeCartesianState.positionY_RTN_m,plant.deputy.initialConditions.relativeCartesianState.positionZ_RTN_m,plant.deputy.initialConditions.relativeCartesianState.velocityX_RTN_m,plant.deputy.initialConditions.relativeCartesianState.velocityY_RTN_m,plant.deputy.initialConditions.relativeCartesianState.velocityZ_RTN_m]',plant.chief.initialConditions.orbitElements.semiMajorAxis_m,plant.environment.earthProperties.gravitationalParameter_m3_s2);
+HCWIntConstantsChief  = computeHCWIntConstants([plant.chief.initialConditions.relativeCartesianState.positionX_RTN_m,plant.chief.initialConditions.relativeCartesianState.positionY_RTN_m,plant.chief.initialConditions.relativeCartesianState.positionZ_RTN_m,plant.chief.initialConditions.relativeCartesianState.velocityX_RTN_m,plant.chief.initialConditions.relativeCartesianState.velocityY_RTN_m,plant.chief.initialConditions.relativeCartesianState.velocityZ_RTN_m]',plant.chief.initialConditions.orbitElements.semiMajorAxis_m,plant.environment.earthProperties.gravitationalParameter_m3_s2);
+
+
+plant.deputy.initialConditions.integrationConstants_HCW.K1 = HCWIntConstantsDeputy(1);
+plant.deputy.initialConditions.integrationConstants_HCW.K2 = HCWIntConstantsDeputy(2);
+plant.deputy.initialConditions.integrationConstants_HCW.K3 = HCWIntConstantsDeputy(3);
+plant.deputy.initialConditions.integrationConstants_HCW.K4 = HCWIntConstantsDeputy(4);
+plant.deputy.initialConditions.integrationConstants_HCW.K5 = HCWIntConstantsDeputy(5);
+plant.deputy.initialConditions.integrationConstants_HCW.K6 = HCWIntConstantsDeputy(6);
+
+plant.chief.initialConditions.integrationConstants_HCW.K1 = HCWIntConstantsChief(1);
+plant.chief.initialConditions.integrationConstants_HCW.K2 = HCWIntConstantsChief(2);
+plant.chief.initialConditions.integrationConstants_HCW.K3 = HCWIntConstantsChief(3);
+plant.chief.initialConditions.integrationConstants_HCW.K4 = HCWIntConstantsChief(4);
+plant.chief.initialConditions.integrationConstants_HCW.K5 = HCWIntConstantsChief(5);
+plant.chief.initialConditions.integrationConstants_HCW.K6 = HCWIntConstantsChief(6);
+
+clear HCWIntConstantsDeputy HCWIntConstantsChief
 
 %% Sim Config
 dt = 6;
@@ -127,11 +150,14 @@ orbitElements       = createBus(plant.chief.initialConditions.orbitElements);
 cartesianState      = createBus(plant.chief.initialConditions.cartesianState);
 relativeCartesianState = createBus(plant.chief.initialConditions.relativeCartesianState);
 orbitElementDifferences = createBus(plant.chief.initialConditions.orbitElementDifferences);
+integrationConstants_HCW = createBus(plant.chief.initialConditions.integrationConstants_HCW);
+
 initialConditions   = createBus(plant.chief.initialConditions);
 initialConditions   = addToBus(initialConditions,"orbitElements","bus");
 initialConditions   = addToBus(initialConditions,"cartesianState","bus");
 initialConditions   = addToBus(initialConditions, "relativeCartesianState", "bus");
 initialConditions   = addToBus(initialConditions,"orbitElementDifferences","bus");
+initialConditions   = addToBus(initialConditions,"integrationConstants_HCW","bus");
 chief               = addToBus(chief,"initialConditions","bus");
 plantBus = addToBus(plantBus,"chief","bus");
 
@@ -139,4 +165,3 @@ plantBus = addToBus(plantBus,"chief","bus");
 deputy              = createBus(plant.chief);
 deputy              = addToBus(deputy,"initialConditions","bus");
 plantBus = addToBus(plantBus,"deputy","bus");
-
--- utilities/functions/computeHCWIntConstants.m
+++ utilities/functions/computeHCWIntConstants.m
@@ -4,13 +4,14 @@ function IC_hcw = computeHCWIntConstants(relativeCartesianState0, a, mu)
 
 n = meanMotion(mu, a);
 
-I_matrix = [a*ones(3), zeros(3); zeros(3), a*n*ones(3)];
+I_matrix = [a*eye(3), zeros(3); zeros(3), a*n*eye(3)];
 t_matrix = [1, 0, 1, 0, 0, 0;
             0, 2, 0, 1, 0, 0;
             0, 0, 0, 0, 0, 1;
             0, 1, 0, 0, 0, 0;
             -3/2, 0, -2, 0, 0, 0;
             0, 0, 0, 0, 1, 0];
+I_matrix*t_matrix
 
 IC_hcw = (I_matrix * t_matrix) \ relativeCartesianState0;
 

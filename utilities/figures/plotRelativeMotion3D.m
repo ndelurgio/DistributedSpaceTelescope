9efcb13b2f9851c16ffe12882f65f1bfe97e31a3
ndelurgio <delurgion@gmail.com>
2023-04-26 10:28:47 -0700

    add 3d velocity

--- utilities/figures/plotRelativeMotion3D.m
+++ utilities/figures/plotRelativeMotion3D.m
@@ -4,19 +4,34 @@ function [] = plotRelativeMotion3D(telem)
 % [~,perigee_idx] = min(abs(time-3.532572227135039e+04));
 
 figure
+set(gcf,'Position',[100 100 700 700])
 % title("Relative Motion in RTN Plane")
+subplot(1,2,1)
+title("Relative 3D Position")
 hold on;
-plot3(telem.("Deputy Relative Cartesian State D'Amico ROE Map.positionX_RTN_m")(2:end),telem.("Deputy Relative Cartesian State D'Amico ROE Map.positionY_RTN_m")(2:end),telem.("Deputy Relative Cartesian State D'Amico ROE Map.positionZ_RTN_m")(2:end),Color="black",LineWidth=1.5)
-plot3(telem.("Deputy Relative Cartesian State RTN YA.positionX_RTN_m")(2:end),telem.("Deputy Relative Cartesian State RTN YA.positionY_RTN_m")(2:end),telem.("Deputy Relative Cartesian State RTN YA.positionZ_RTN_m")(2:end))
+plot3(telem.("Deputy Relative Cartesian State RTN Nonlinear.positionX_RTN_m")(2:end),telem.("Deputy Relative Cartesian State RTN Nonlinear.positionY_RTN_m")(2:end),telem.("Deputy Relative Cartesian State RTN Nonlinear.positionZ_RTN_m")(2:end),LineWidth=1.5)
+plot3(telem.("Deputy Relative Cartesian State D'Amico ROE Map.positionX_RTN_m")(2:end),telem.("Deputy Relative Cartesian State D'Amico ROE Map.positionY_RTN_m")(2:end),telem.("Deputy Relative Cartesian State D'Amico ROE Map.positionZ_RTN_m")(2:end),LineWidth=1.5)
+plot3(telem.("Deputy Relative Cartesian State RTN YA.positionX_RTN_m")(2:end),telem.("Deputy Relative Cartesian State RTN YA.positionY_RTN_m")(2:end),telem.("Deputy Relative Cartesian State RTN YA.positionZ_RTN_m")(2:end),LineWidth=1.5)
 
 axis equal;
 xlabel("\delta r_r [m]")
 ylabel("\delta r_t [m]")
 zlabel("\delta r_n [m]")
 view(30,30)
+legend(["Nonlinear","Geometric","YA"])
+grid on;
+subplot(1,2,2)
+title("Relative 3D Velocity")
 hold on;
-% scatter3(telem.("relativeState")(perigee_idx,1),telem.("relativeState")(perigee_idx,2),telem.("relativeState")(perigee_idx,3),"MarkerFaceColor","g")
-% scatter3(telem.("relativeState")(apogee_idx,1),telem.("relativeState")(apogee_idx,2), telem.("relativeState")(apogee_idx,3),"MarkerFaceColor","r")
-legend(["Geometric","YA"])
+plot3(telem.("Deputy Relative Cartesian State RTN Nonlinear.velocityX_RTN_m")(2:end),telem.("Deputy Relative Cartesian State RTN Nonlinear.velocityY_RTN_m")(2:end),telem.("Deputy Relative Cartesian State RTN Nonlinear.velocityZ_RTN_m")(2:end),LineWidth=1.5)
+plot3(telem.("Deputy Relative Cartesian State D'Amico ROE Map.velocityX_RTN_m")(2:end),telem.("Deputy Relative Cartesian State D'Amico ROE Map.velocityY_RTN_m")(2:end),telem.("Deputy Relative Cartesian State D'Amico ROE Map.velocityZ_RTN_m")(2:end),LineWidth=1.5)
+plot3(telem.("Deputy Relative Cartesian State RTN YA.velocityX_RTN_m")(2:end),telem.("Deputy Relative Cartesian State RTN YA.velocityY_RTN_m")(2:end),telem.("Deputy Relative Cartesian State RTN YA.velocityZ_RTN_m")(2:end),LineWidth=1.5)
+
+axis equal;
+xlabel("\delta v_r [m]")
+ylabel("\delta v_t [m]")
+zlabel("\delta v_n [m]")
+view(30,30)
+legend(["Nonlinear","Geometric","YA"])
 grid on;
 end

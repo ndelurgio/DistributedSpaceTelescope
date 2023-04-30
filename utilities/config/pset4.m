2ab812827170b647aca87ac7d1d47971450a34e7
ndelurgio <delurgion@gmail.com>
2023-04-30 16:44:11 -0700

    push

---
+++ utilities/config/pset4.m
@@ -0,0 +1,8 @@
+%% Config File
+chiefOE = [7000e3, 0.001, deg2rad(59), deg2rad(84), deg2rad(188), deg2rad(180.1)];
+aROE = [0, 100, 50, 100, 30, 200];
+
+deputyOE = [36943e3, 0.81111, deg2rad(59.005), deg2rad(84), deg2rad(188), deg2rad(180.1)];
+J2_flag = 1;
+
+loadSimConfig;
---
+++ utilities/functions/damicoROE2deputyOE.m
@@ -0,0 +1,10 @@
+function deputyOE = damicoROE2deputyOE(ROE,chiefOE)
+a = chiefOE(1);
+e = chiefOE(2);
+i = chiefOE(3);
+O = chiefOE(4);
+w = chiefOE(5);
+M = chief
+
+end
+

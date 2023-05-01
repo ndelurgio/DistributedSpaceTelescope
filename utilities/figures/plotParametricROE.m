1423dd860b9b80a70450406c391ce017bfce1c7f
ndelurgio <delurgion@gmail.com>
2023-04-30 18:06:39 -0700

    bug fixes

--- utilities/figures/plotParametricROE.m
+++ utilities/figures/plotParametricROE.m
@@ -2,6 +2,9 @@ a = plant.chief.initialConditions.meanOrbitElements.semiMajorAxis_m;
 figure
 hold on;
 set(gca,"LineWidth",3)
+set(gca,"GridLineWidth",1)
 plot(a*telem.("Osculating D'Amico ROE True.relativeLongitude"),a*telem.("Osculating D'Amico ROE True.relativeSemiMajorAxis"),LineWidth=2)
 axis equal;
 grid on;
+ylabel('$a\delta a$','Interpreter','latex',"FontSize",20)
+xlabel('$a\delta \lambda$','Interpreter','latex',"FontSize",20)
--- utilities/functions/singular2quasiOE.m
+++ utilities/functions/singular2quasiOE.m
@@ -1,6 +1,6 @@
 function OE_quasi = singular2quasiOE(OE)
 % Singular OEs to quasi-nonsingular OEs
-
+OE_quasi = zeros(6,1);
 OE_quasi(1) = OE(1);
 OE_quasi(2) = wrapTo2Pi(OE(5) + OE(6));
 OE_quasi(3) = OE(2) * cos(OE(5));

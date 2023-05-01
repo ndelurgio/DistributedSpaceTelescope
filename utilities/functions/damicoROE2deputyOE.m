65eeffe0a31bc312a0eacb710f9e9291abd308f6
ndelurgio <delurgion@gmail.com>
2023-04-30 17:01:09 -0700

    added ROE 2 Deputy OE

--- utilities/functions/damicoROE2deputyOE.m
+++ utilities/functions/damicoROE2deputyOE.m
@@ -1,10 +1,27 @@
 function deputyOE = damicoROE2deputyOE(ROE,chiefOE)
+
 a = chiefOE(1);
-e = chiefOE(2);
-i = chiefOE(3);
-O = chiefOE(4);
-w = chiefOE(5);
-M = chief
+ex = chiefOE(2);
+ey = chiefOE(3);
+i = chiefOE(4);
+O = chiefOE(5);
+u = chiefOE(6);
+
+da = ROE(1);
+dlam = ROE(2);
+dex = ROE(3);
+dey = ROE(4);
+dix = ROE(5);
+diy = ROE(6);
+
+ad = a*da + a;
+edx = dex + ex;
+edy = dey + ey;
+id = dix + i;
+Od = diy/sin(i) + O;
+ud = dlam + u -(Od-O)*cos(i);
+
+deputyOE = [ad,edx,edy,id,Od,ud];
 
 end
 

08ddfcc36e49da358d7f9afa8adf67ef271c730e
ndelurgio <delurgion@gmail.com>
2023-05-09 11:53:13 -0700

    update gitignore

--- utilities/config/nominalConfig.m
+++ utilities/config/nominalConfig.m
@@ -1,8 +1,9 @@
 %% Config File
 chiefOE = [36943e3, 0.8111, deg2rad(59), deg2rad(84), deg2rad(188), deg2rad(180.1)];
 % deputyOE = [36943e3, 0.81111, deg2rad(59.002), deg2rad(84), deg2rad(188), deg2rad(180.1)];
-ROE = [0,0,0,1.2903e-06,0,3.4317e-06];
-
+ROE = [0;0;0;0;3.4317e-06;0];
+deputyOE = eccentricROE2deputyOE(ROE, chiefOE);
+%1.2903e-06
 t_epoch = datetime(2024,6,1);
 t_final = t_epoch + days(2);
 J2_flag = 1;

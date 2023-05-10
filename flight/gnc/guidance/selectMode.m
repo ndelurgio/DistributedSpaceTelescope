f6889c4c197197199ccac590f6bfcd6896451f66
Hugo Buurmeijer <hugo.buurmeijer@gmail.com>
2023-05-09 18:44:53 -0700

    Create mode selector model and guidance initial model

---
+++ flight/gnc/guidance/selectMode.m
@@ -0,0 +1,15 @@
+function mode = selectMode(nu, scienceMode, formationBreakMode, passiveMode, formationAcquisitionMode)
+% Select the correct mode based on the true anomaly (nu)
+if scienceMode(1) < nu < scienceMode(2)
+    mode = "science";
+elseif formationBreakMode(1) < nu < formationBreakMode(2)
+    mode = "formationBreak";
+elseif passiveMode(1) < nu < passiveMode(2)
+    mode = "passive";
+elseif formationAcquisitionMode(1) < nu < formationAcquisitionMode(2)
+    mode = "formationAcquisition";
+else
+    mode = "unknown";
+end
+
+end

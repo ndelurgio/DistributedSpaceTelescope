88ff754dffe95be172afad2595c9e58482f68de7
Hugo Buurmeijer <hugo.buurmeijer@gmail.com>
2023-05-09 22:33:38 -0700

    Update guidance

--- flight/gnc/guidance/mode2maneuver.m
+++ flight/gnc/guidance/mode2maneuver.m
@@ -1,15 +1,18 @@
-function pseudoState = mode2maneuver(mode, ROE, desiredInclination)
+function [pseudoState, maneuverLocations] = mode2maneuver(mode, chiefOE, ROE, desiredInclination)
 % Find the maneuver that suits the mode described by a pseudostate
 
 pseudoState = zeros(6, 1);
+maneuverLocations = [];
 
 if mode == "formationBreak"
+    nu = chiefOE(6);
     if abs(ROE(1)) > eps
         pseudoState(1) = -ROE(1);
-    elseif abs(ROE(5) - desiredInclination(1)) > eps
+        maneuverLocations = [maneuverLocations, nu];
+    elseif abs(ROE(5) - desiredInclination(1)) > eps || abs(ROE(6) - desiredInclination(2)) > eps
         pseudoState(5) = desiredInclination(1);
-    elseif abs(ROE(6) - desiredInclination(2)) > eps
         pseudoState(6) = desiredInclination(2);
+        maneuverLocations = [maneuverLocations, nu];
     end
 end
 % TODO: other modes

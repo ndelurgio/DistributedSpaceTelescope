93819d5158e60b5408cde41f83397977e7eb97f9
ndelurgio <delurgion@gmail.com>
2023-05-24 08:50:05 -0700

    sim for pset7

--- utilities/config/pset7.m
+++ utilities/config/pset7.m
@@ -15,13 +15,13 @@ M0 = nu2M(nu0,chiefOE(2));
 Mf = nu2M(nuf,chiefOE(2));
 n = meanMotion(3.986004419e14,chiefOE(1));
 tdur = (Mf-M0)/n;
-t_final = t_epoch + seconds(tdur);
-J2_flag = 0;
-srp_flag = 0;
+t_final = t_epoch + seconds(tdur) + days(0.2);
+J2_flag = 1;
+srp_flag = 1;
 
 [sunPosData, sunTimeData] = generateSunEphemeris(t_epoch,t_final,60);
 sunPos = getSunPosition(sunPosData, sunTimeData, 1);
-delta_t = 6;
+delta_t = 1;
 % chiefOE = mean2osc(chiefOE,J2_flag);
 % [rChiefECI, vChiefECI] = oe2eci(chiefOE(1), chiefOE(2), chiefOE(3), chiefOE(4), chiefOE(5), chiefOE(6), mu);
 % 

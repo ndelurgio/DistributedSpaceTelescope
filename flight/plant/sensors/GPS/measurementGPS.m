8ecdd35ad7b40b628c28801e1e2ea5e500df787f
Hugo Buurmeijer <hugo.buurmeijer@gmail.com>
2023-05-28 16:37:48 -0700

    Add noises to GPS

--- flight/plant/sensors/GPS/measurementGPS.m
+++ flight/plant/sensors/GPS/measurementGPS.m
@@ -1,15 +1,13 @@
-function [yR, yV] = measurementGPS(cartesianState, biasRnew, biasVnew, sigmaR, sigmaV)
+function [yR, yV] = measurementGPS(cartesianState, biasRnew, biasVnew, sigmaR, sigmaV, noiseR, noiseV)
 % The measurement model for the GPS sensor
 
 r = cartesianState(1:3,1);
 v = cartesianState(4:6,1);
 
-rng('default')  % For reproducibility
-mu = zeros(3, 1);
-errorR = mvnrnd(mu, sigmaR)';
-errorV = mvnrnd(mu, sigmaV)';
+noiseR = sqrtm(sigmaR) * noiseR;
+noiseV = sqrtm(sigmaV) * noiseV;
 
-yR = r + biasRnew + errorR;
-yV = v + biasVnew + errorV;
+yR = r + biasRnew + noiseR;
+yV = v + biasVnew + noiseV;
 
 end

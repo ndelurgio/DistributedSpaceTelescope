16c10a60ec6557231f93af8a19f11e7404dbf44d
ndelurgio <delurgion@gmail.com>
2023-05-29 15:40:37 -0700

    add A jacobian

--- flight/gnc/estimation/ekf.m
+++ flight/gnc/estimation/ekf.m
@@ -1,18 +1,19 @@
-function [mu,cov] = ekf(mu_prev, cov_prev, y, M, J, dt, Q, R)
+function [mu,cov] = ekf(mu_prev,cov_prev, y, u, Bsrp_c, Bsrp_d, sunPos, dt, Q, R)
+
+% CONSTANTS
+mu_earth = 3.986004419e14;
+J2 = 0.108263e-2;
+Re = 6.378e6;
+AU = 1.496e11;
+p_sr = 4.5344321e-6;
 
 % Predict
-mu = zeros(10,1);
-mu(1:7,1) = attitudeDynamicsEuler(mu_prev(1:7,1),M,J,dt);
-mu(1:4,1) = mu(1:4,1)/norm(mu(1:4,1));
-mu(8:10,1) = gyroBiasEuler(mu_prev(8:10,1),[0;0;0],dt);
-q = mu(1:4);
-y_qvec = y(1:3);
-y_wvec = y(5:7);
-A = jacobianest(@(x) [attitudeDynamicsEuler(x(1:7),M,J,dt); gyroBiasEuler(x(8:10),zeros(3,1),dt)], mu_prev);
+mu = dynamicsModel(mu_prev,u,sunPos,Bsrp_c,Bsrp_d,dt,mu_earth,J2,Re,AU,p_sr);
+A = getA(mu_prev,u,sunPos,Bsrp_c,Bsrp_d,dt,mu_earth,J2,Re,AU,p_sr);
 cov = A*cov_prev*A' + Q;
 % Kalman Gain
-C = jacobianest(@(x) getSensors(x,R(5:7,5:7),R(1:4,1:4)),mu);
-K = cov*C'*inv(C*cov*C'+R);
+H = getC
+K = cov*H'*inv(H*cov*H'+R);
 % Expected Measurement
 Ey = getSensors(mu,zeros(3,3),zeros(4,4));
 mu = mu + K*(y-Ey);
